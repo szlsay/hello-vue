dbad4131fb99796affbb66e0ad86d40e3da803fa
小沈曰 <409178030@qq.com>
2022-02-03 16:07:44 +0800

    vue03

--- LearnVuejs01/08-书籍购物车案例/main.js
+++ LearnVuejs01/08-书籍购物车案例/main.js
@@ -48,14 +48,32 @@ const app = new Vue({
   },
   computed: {
     totalPrice() {
-      let totalPrice = 0
-      for (let i = 0; i < this.books.length; i++) {
-        totalPrice += this.books[i].price * this.books[i].count
-      }
-      return totalPrice
+      // 1.普通的for循环
+      // let totalPrice = 0
+      // for (let i = 0; i < this.books.length; i++) {
+      //   totalPrice += this.books[i].price * this.books[i].count
+      // }
+      // return totalPrice
 
-      // for (let i in/of this.books)
-      // reduce
+      // 2.for (let i in this.books)
+      // let totalPrice = 0
+      // for (let i in this.books) {
+      //   const book = this.books[i]
+      //   totalPrice += book.price * book.count
+      // }
+      //
+      // return totalPrice
+
+      // 3.for (let i of this.books)
+      // let totalPrice = 0
+      // for (let item of this.books) {
+      //   totalPrice += item.price * item.count
+      // }
+      // return totalPrice
+
+      return this.books.reduce(function (preValue, book) {
+        return preValue + book.price * book.count
+      }, 0)
     }
   },
   filters: {
@@ -64,3 +82,8 @@ const app = new Vue({
     }
   }
 })
+
+
+
+
+

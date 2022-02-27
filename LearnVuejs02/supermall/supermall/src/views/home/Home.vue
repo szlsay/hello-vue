1cf7213f07b5dcf0874d7f1cc84826c2047b466e
小沈曰 <409178030@qq.com>
2022-02-27 22:23:04 +0800

    修复滚动

--- LearnVuejs02/supermall/supermall/src/views/home/Home.vue
+++ LearnVuejs02/supermall/supermall/src/views/home/Home.vue
@@ -126,6 +126,10 @@
     mounted(){
 
       //1.图片加载完成的事件监听
+      const refresh = debounce(this.$refs.scroll.refresh, 200)
+      this.$bus.$on('itemImageLoad', () => {
+        refresh()
+      })
       // const refresh=this.debounce(this.$refs.scroll.refresh,200)
       // const refresh=debounce(this.$refs.scroll.refresh,200)
 
